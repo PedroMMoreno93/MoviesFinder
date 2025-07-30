3ce37981d73d122cba2e6aa5e6ada11610ce5b82
Pedro M Moreno <morenogonzalezpedro93@gmail.com>
2025-07-30 16:33:02 +0200

    (feature): project creation

---
+++ MoviesFinder/MoviesFinder/MoviesFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesFinderApp.swift
+//  MoviesFinder
+//
+//  Created by Pedro M Moreno on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
