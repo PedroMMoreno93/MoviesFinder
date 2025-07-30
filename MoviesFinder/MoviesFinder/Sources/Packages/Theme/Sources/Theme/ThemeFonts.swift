4f9ec4f28a8bb2abd2ae7cdcef6e84a10509c3ac
Pedro M Moreno <morenogonzalezpedro93@gmail.com>
2025-07-30 18:14:40 +0200

    (refactor): indentation

--- MoviesFinder/MoviesFinder/Sources/Packages/Theme/Sources/Theme/ThemeFonts.swift
+++ MoviesFinder/MoviesFinder/Sources/Packages/Theme/Sources/Theme/ThemeFonts.swift
@@ -10,16 +10,22 @@ import SwiftUI
 public enum ThemeFonts {
     /// Primary screen headers and main titles
     public static let display = Font.system(.largeTitle).weight(.semibold)
+    
     /// Section headings in lists or panels
     public static let title = Font.system(.title2).weight(.semibold)
+    
     /// Titles in list items
     public static let headline = Font.system(.headline).weight(.semibold)
+    
     /// Paragraph text such as synopses
     public static let body = Font.system(.body).weight(.regular)
+    
     /// Secondary text under headlines (dates, ratings)
     public static let subhead = Font.system(.subheadline).weight(.regular)
+    
     /// Small labels, timestamps, credits
     public static let caption = Font.system(.caption).weight(.regular)
+    
     /// Copyright notes, disclaimers
     public static let footnote = Font.system(.footnote).weight(.regular)
 }
