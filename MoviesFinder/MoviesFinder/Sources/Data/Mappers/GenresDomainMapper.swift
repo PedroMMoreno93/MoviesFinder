b47b52bc1464c8d937bda841b787c3e8691443a8
Pedro M Moreno <morenogonzalezpedro93@gmail.com>
2025-07-31 19:22:38 +0200

    (feature): implemented mappers

---
+++ MoviesFinder/MoviesFinder/Sources/Data/Mappers/GenresDomainMapper.swift
@@ -0,0 +1,22 @@
+//
+//  GenresDomainMapper.swift
+//  MoviesFinder
+//
+//  Created by Pedro M Moreno on 31/7/25.
+//
+
+open class GenresDomainMapper {
+    func map(
+        responseModel: GenresListResponseModel
+    ) -> [GenreEntity] {
+        let mappedEntities = responseModel.genres.compactMap { item in
+            if let id = item.id,
+               let name = item.name {
+                return GenreEntity(id: id, name: name)
+            } else {
+                return nil
+            }
+        }
+        return mappedEntities
+    }
+}
---
+++ MoviesFinder/MoviesFinder/Sources/Data/Mappers/MoviesDomainMapper.swift
@@ -0,0 +1,58 @@
+//
+//  MoviesDomainMapper.swift
+//  MoviesFinder
+//
+//  Created by Pedro M Moreno on 30/7/25.
+//
+
+import Foundation
+
+open class MoviesDomainMapper {
+    func map(
+        responseModel: MovieListResponseModel
+    ) -> [MoviesListItemEntity] {
+        let mappedEntities = responseModel.results.compactMap { item in
+            let backdropPathURL: String?
+            
+            if let backdropPath = item.backdropPath {
+                backdropPathURL = DataConstants.URLs.imagesBaseURL + backdropPath
+            } else {
+                backdropPathURL = nil
+            }
+            
+            let posterPathURL: String?
+
+            if let posterPath = item.posterPath {
+                posterPathURL = DataConstants.URLs.imagesBaseURL + posterPath
+            } else {
+                posterPathURL = nil
+            }
+            
+            return MoviesListItemEntity(
+                adult: item.adult,
+                backdropPath: backdropPathURL,
+                genreIDS: item.genreIDS ?? [],
+                id: item.id,
+                originalLanguage: item.originalLanguage,
+                originalTitle: item.originalTitle,
+                overview: item.overview,
+                popularity: item.popularity ?? 0,
+                posterPath: posterPathURL,
+                releaseDate: item.releaseDate,
+                title: item.title,
+                video: item.video ?? false,
+                voteAverage: item.voteAverage ?? 0,
+                voteCount: item.voteCount ?? 0
+            )
+        }
+        return mappedEntities
+    }
+    
+    func mapGenresIds(
+        _ genres: [GenreEntity]
+    ) -> [String] {
+        return genres.compactMap { genre in
+            return "\(genre.id)"
+        }
+    }
+}
